ad9b7a080f2930c1cf0ebe48b21e5524c5cca189
Jimmy Quach <jminquach@gmail.com>
2022-11-12 01:17:06 -0800

    fix: change Client refs to Peer

--- examples/test/main.go
+++ examples/test/main.go
@@ -14,20 +14,20 @@ func main() {
 
 	server := gecgosio.Gecgos(nil)
 
-	server.OnConnection(func(c gecgosio.Client) {
-		fmt.Printf("Client %s has connected!\n", c.Id)
+	server.OnConnection(func(peer gecgosio.Peer) {
+		fmt.Printf("Client %s has connected!\n", peer.Id)
 
 		// Example of sending and recieving from client(s)
 		// Server will recieve the event 'ping' with data 'hello'
 		// Server will send the event 'pong' with data 'world'
-		c.On("ping", func(msg string) {
-			fmt.Printf("Client %s sent event 'ping' with data '%s', emitting back 'pong'\n", c.Id, msg)
-			c.Emit("pong", "world")
+		peer.On("ping", func(msg string) {
+			fmt.Printf("Client %s sent event 'ping' with data '%s', emitting back 'pong'\n", peer.Id, msg)
+			peer.Emit("pong", "world")
 		})
 	})
 
-	server.OnDisconnect(func(c gecgosio.Client) {
-		fmt.Printf("Client %s has disconnected!\n", c.Id)
+	server.OnDisconnect(func(peer gecgosio.Peer) {
+		fmt.Printf("Client %s has disconnected!\n", peer.Id)
 	})
 
 	server.Listen(420)
