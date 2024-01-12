24e2fa0475dfd6f36bff428b6433274a6d0a86d3
Jimmy Quach <jminquach@gmail.com>
2024-01-12 01:21:53 -0800

    feat: add default ice servers

--- sdp.go
+++ sdp.go
@@ -18,7 +18,16 @@ func (s *Server) createConnection(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Client attempting to connect from: ", r.RemoteAddr)
 
 	// Create a new RTCPeerConnection
-	peerConnection, err := api.NewPeerConnection(webrtc.Configuration{})
+	peerConnection, err := api.NewPeerConnection(webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{URLs: []string{"stun:stun.l.google.com:19302"}},
+			{URLs: []string{"stun:stun1.l.google.com:19302"}},
+			{URLs: []string{"stun:stun2.l.google.com:19302"}},
+			{URLs: []string{"stun:stun3.l.google.com:19302"}},
+			{URLs: []string{"stun:stun4.l.google.com:19302"}},
+		},
+		SDPSemantics: webrtc.SDPSemanticsUnifiedPlanWithFallback,
+	})
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
